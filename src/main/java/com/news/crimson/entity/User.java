8f39bfea16e3e7b0b41c80579ffa412761ef2231
LAPTOP-4TUIFEDC\ragapriya <ragapriya.saravanan14@gmail.com>
2022-10-25 15:33:17 -0400

    [LC-90] - DEV - Skeletal design for user profile

--- src/main/java/com/news/crimson/entity/User.java
+++ src/main/java/com/news/crimson/entity/User.java
@@ -1,5 +1,21 @@
 package com.news.crimson.entity;
 
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class User {
+
+	public Integer id;	
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String phoneNo;
+	public String location;
+	public List<String> favCategory;
+	public List<Object> friends;
+	public List<Object> followers;
+	public List<Object> blocked;	
 
 }
