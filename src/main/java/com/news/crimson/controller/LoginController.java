d8eba07f7176d366aea996db4fc15aaa6ce28f68
_KK_\kiran <kirankishoregk@gmail.com>
2022-10-25 16:14:48 -0400

    feat: [LC-88] skeletal design for login

--- src/main/java/com/news/crimson/controller/LoginController.java
+++ src/main/java/com/news/crimson/controller/LoginController.java
@@ -1,13 +1,21 @@
 package com.news.crimson.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.news.crimson.entity.User;
+import com.news.crimson.service.LoginService;
 
 @RestController
 public class LoginController {
 	
+	@Autowired
+	private LoginService loginService;
+	
 	@PostMapping(path = "/login")
 	public String login() {
+		User user = loginService.loginAccount();
 		return "login success";
 	}
 }
--- src/main/java/com/news/crimson/service/LoginService.java
+++ src/main/java/com/news/crimson/service/LoginService.java
@@ -1,5 +1,19 @@
 package com.news.crimson.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.news.crimson.entity.User;
+
+@Service
 public class LoginService {
+	
+	@Autowired
+	private ProfileService profileService;
+	
+	public User loginAccount() {
+		User user = profileService.getProfileDetails();
+		return user;
+	}
 
 }
