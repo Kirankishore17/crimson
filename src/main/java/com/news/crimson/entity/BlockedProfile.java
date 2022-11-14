b9028af538d4378d21bd97db38c7106e25084789
_KK_\kiran <kirankishoregk@gmail.com>
2022-11-13 23:17:06 -0500

    feat: Updated profile entity structure

--- src/main/java/com/news/crimson/entity/BlockedProfile.java
+++ src/main/java/com/news/crimson/entity/BlockedProfile.java
@@ -15,11 +15,14 @@ import lombok.Data;
 public class BlockedProfile {
 
 	@Id
-//	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name = "blocked_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "BLOCKED_ID")
 	private Integer id;
+	
+	@Column(name = "USER_ID")
 	private Integer userId;
+	
+	@Column(name = "BLOCKED_PROFILE_NAME")
 	private String blockedProfileName;
-
 
 }
