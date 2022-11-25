8fbce84d34485a55da6ca14e55b2d18fe0e55cbd
_KK_\kiran <kirankishoregk@gmail.com>
2022-11-25 18:50:22 -0500

    fix: Included reference image in news API response

--- src/main/java/com/news/crimson/model/NewsInfo.java
+++ src/main/java/com/news/crimson/model/NewsInfo.java
@@ -1,14 +1,5 @@
 package com.news.crimson.model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.news.crimson.model.newsapi.NewsBody;
-import com.news.crimson.model.newsapi.NewsItem;
-
 import lombok.Data;
 
 @Data
@@ -21,30 +12,6 @@ public class NewsInfo {
 	private String articleUrl;
 	private String publishDate;
 	private String description;
-
-	public List<NewsInfo> getNewsInfoListFromRssNews(NewsBody rssNewsBody) {
-		List<NewsInfo> news = new ArrayList<>();
-		if(rssNewsBody.getChannel() != null && rssNewsBody.getChannel().getItem() != null) {
-			List<NewsItem> contentList = rssNewsBody.getChannel().getItem();
-			int count = 1;
-			for (NewsItem item : contentList) {
-				NewsInfo info = new NewsInfo();
-				info.setId(count);
-				ObjectMapper objectMapper = new ObjectMapper();
-				Map<String, String> source = objectMapper.convertValue(item.getSource(), HashMap.class);
-				if(source.keySet().contains("url"))
-						info.setSourceUrl(source.get("url"));
-				if(source.keySet().contains(""))
-						info.setSourceName(source.get(""));
-				String title = item.getTitle().substring(0, item.getTitle().length() - (2+source.get("").length()));
-				info.setHeadlines(title);
-				info.setDescription(item.getDescription());
-				info.setPublishDate(item.getPubDate());
-				info.setArticleUrl(item.getLink());
-				count++;
-				news.add(info);
-			}
-		}
-		return news;
-	}
+	private String cover;
+	
 }
